bdd3f52c24aa4ede3828d074fbea51158c71c405
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-07-13 09:07:30 +0900

    サインアップ：vee-validate実装

--- src/components/pages/auth/Signup.vue
+++ src/components/pages/auth/Signup.vue
@@ -1,92 +1,98 @@
 <template>
     <div class="auth">
-       <div>
-          <v-card class="card">
-            <v-card-title>
-                ユーザー登録
-            </v-card-title>
-            <div class="px-4">
-                <div class="py-4" v-if="error">
-                    <v-alert
-                        dense
-                        outlined
-                        type="error"
-                    >
-                        {{ error }}
-                    </v-alert>
-                </div>
-                <div class="py-4" v-if="loading">
-                    <v-progress-linear
-                        indeterminate
-                        color="primary"
-                    ></v-progress-linear>
-                </div>
-                <v-form
-                    ref="form"
-                    v-model="valid"
-                    lazy-validation
-                >
-                    
-                    <div>
-                        <v-text-field
-                            v-model="email"
-                            :rules="emailRules"
-                            label="E-mail"
-                            outlined
+        <ExecLoading v-if="loading" />
+        <div>
+            <v-card class="card">
+                <v-card-title>
+                    ユーザー登録
+                </v-card-title>
+                <div class="px-4">
+                    <div class="py-4" v-if="error">
+                        <v-alert
                             dense
-                            required
-                        ></v-text-field>
-                    </div>
-                
-                    <div>
-                        <v-text-field
-                            type="password"
-                            label="パスワード"
-                            :rules="passwordRules"
-                            required
                             outlined
-                            dense
-                            v-model="password"
-                        ></v-text-field>
+                            type="error"
+                        >
+                            {{ error }}
+                        </v-alert>
                     </div>
-                </v-form>
-            </div>
-            <div class="pa-4">
-               <v-btn
-                    :disabled="!valid"
-                    color="primary"
-                    class="submit"
-                    @click="signup()"
-                >
-                    登録する
-                </v-btn>
-            </div>
-            <v-divider />
-            <div class="pa-4">
-                <v-btn outlined color="primary" to="/auth/signin">
-                    ログインはこちら
-                </v-btn>
-            </div>
-          </v-card>
+                    <validation-observer v-slot="{ invalid }" ref="observer">
+                        <validation-provider
+                            name="メールアドレス"
+                            :rules="{
+                                email: {},
+                                required: {}
+                            }"
+                            v-slot="{ errors }"
+                            tag="div"
+                        >
+                            <v-text-field
+                                v-model="email"
+                                label="メールアドレス"
+                                hide-details
+                                outlined
+                                dense
+                            ></v-text-field>
+                            <div class="input-error-messsage">{{ errors[0] }}</div>
+                        </validation-provider>
+                        <validation-provider
+                            name="パスワード"
+                            :rules="{
+                                required: {},
+                                min: max_length_password
+                            }"
+                            v-slot="{ errors }"
+                            tag="div"
+                            class="mt-6"
+                        >
+                            <v-text-field
+                                label="パスワード"
+                                hide-details
+                                type="password"
+                                outlined
+                                dense
+                                v-model="password"
+                            ></v-text-field>
+                            <div
+                                v-for="(error, index) in errors"
+                                :key="index"
+                                class="input-error-messsage"
+                            >
+                                {{ error }}
+                            </div>
+                        </validation-provider>
+                        <div class="my-6">
+                            <v-btn
+                                :disabled="invalid"
+                                color="primary"
+                                class="submit"
+                                @click="signup()"
+                            >
+                                登録する
+                            </v-btn>
+                        </div>
+                    </validation-observer>
+                </div>
+                <v-divider />
+                <div class="pa-4">
+                    <v-btn outlined color="primary" to="/auth/signin">
+                        ログインはこちら
+                    </v-btn>
+                </div>
+            </v-card>
         </div>
     </div>
 </template>
 
 <script>
-
+import ExecLoading from "@/components/common/ExecLoading.vue"
 export default {
+    components: {
+        ExecLoading
+    },
     data: () => ({
-        valid: true,
         email: '',
-        emailRules: [
-        v => !!v || 'メールアドレスは入力必須です',
-        v => /.+@.+\..+/.test(v) || 'メールアドレスの形式で入力してください',
-        ],
-        passwordRules: [
-            v => !!v || 'パスワードは入力必須です',
-        ],
         password: '',
-
         loading: false,
         error: '',
     }),
@@ -95,23 +101,19 @@ export default {
         // サインアップ
         async signup () {
             this.loading = true
-            const valid = this.$refs.form.validate();
-            if(valid) {
+            try {
                 const uid = await this.firebaseSignup(this.email, this.password)
                 if(uid) {
                     this.storeSetFirebaseUid(uid);
                     this.pageMove('/account')
                 } else {
-                    this.error = "入力されたメールアドレスの形式が間違っているか、すでに登録されている可能性があります。"
+                    throw new Error()
                 }
+            } catch (error) {
+                console.log(error);
+                this.error = "入力されたメールアドレスのユーザーはすでに登録されている可能性があります。"
             }
             this.loading = false;
-        },
-        reset () {
-            this.$refs.form.reset()
-        },
-        resetValidation () {
-            this.$refs.form.resetValidation()
         },
     },
 }
