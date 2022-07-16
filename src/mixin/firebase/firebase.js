9dc446f4f85863a226817c9a058dcb057e51eb73
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-07-16 21:34:28 +0900

    firebase不要ファイル削除

--- src/mixin/firebase/firebase.js
+++
@@ -1,8 +0,0 @@
-// Firebase
-import firebaseSettings from "@/config/settings/firebase.js"
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from "firebase/database";
-const firebaseApp = initializeApp(firebaseSettings.firebaseConfig);
-getDatabase(firebaseApp);
-
-console.log('hi');
