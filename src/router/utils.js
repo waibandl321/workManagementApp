0b93d75c11fd49f5008f901742b4bdcee14d9d4f
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-07-16 21:54:06 +0900

    router共有：未使用関数削除

--- src/router/utils.js
+++ src/router/utils.js
@@ -1,17 +1,8 @@
 import storeAuth from '@/mixin/store/auth.js'
-import storeAccount from '@/mixin/store/account.js'
+// import storeAccount from '@/mixin/store/account.js'
 
 export default {
     isSignin() {
         return storeAuth.methods.storeGetFirebaseUid()
     },
-    isExistAccount() {
-        const result = storeAccount.methods.storeGetAccountInfo()
-        if(result.first_name) {
-            return result
-        } else {
-            return false
-        }
-        
-    }
 }
