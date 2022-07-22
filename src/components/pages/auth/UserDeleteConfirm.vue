b931ad1296f7384df44eb335d4636377ed50d5da
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-07-22 10:28:44 +0900

    アカウント削除 遷移修正

--- src/components/pages/auth/UserDeleteConfirm.vue
+++ src/components/pages/auth/UserDeleteConfirm.vue
@@ -1,36 +1,15 @@
 <template>
-    <div class="auth">
-        <v-container>
-            <v-card width="100%" class="pa-6">
-                <v-alert type="error">
-                    アカウントを削除しました。
-                </v-alert>
-                <v-card-actions
-                    class="mt-2 justify-center"
-                >
-                    <v-btn
-                        @click="finishApp()"
-                        x-large
-                        color="#333"
-                        dark
-                        width="100%"
-                        data-e2e-id="finishApp"
-                    >
-                        アプリケーションの利用を終了する
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-container>
-    </div>
+    <div class="auth"></div>
 </template>
 
 <script>
 export default {
-    methods: {
-        finishApp() {
+    mounted() {
+        setTimeout(() => {
+            this.openAlert('アカウントを削除しました。アプリケーションの利用を終了します。')
             this.$router.push("/auth/signout")
-        }
-    }
+        }, 100)
+    },
 }
 </script>
 <style>
